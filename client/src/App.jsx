96f39fc7d9f0e199a871a00c0fe375911173acb7
tweppy <tweppy@gmail.com>
2023-10-30 18:01:06 +0100

    add fetch todos for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+
+const API_BASE = "http://localhost:4000/api";
+
 function App() {
-  return <></>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await fetch(API_BASE + "/todos");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const json = await response.json();
+      const data = json.todos;
+
+      if (response.ok) {
+        setTodos(data);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {todos.map((todo) => (
+          <p key={todo._id}>{todo.text}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
